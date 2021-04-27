6d329efd1831df823ce930670aab3da213523046
Udayraj Mandke <urmandke@gmail.com>
2021-04-27 19:09:03 +0530

    Hello Updated

--- Hello_World/hello.go
+++ Hello_World/hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 //Helllo returns Hello, world string
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
