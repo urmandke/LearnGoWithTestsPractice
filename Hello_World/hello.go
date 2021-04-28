dc685e4fe5ae428423fe389ce2bc7fb53a26ae15
Udayraj Mandke <urmandke@gmail.com>
2021-04-28 10:46:10 +0530

    Integers Completed

--- Hello_World/hello.go
+++ Hello_World/hello.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 
 //Helllo returns Hello, world string
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
